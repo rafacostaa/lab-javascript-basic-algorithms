c8876ee83568f42aada07d800c02f40f8209e30e
rafaelrgc <46941563+rafaelrgc@users.noreply.github.com>
2019-02-11 21:30:12 -0200

    Add files via upload

---
+++ lab-javascript-basic-algorithms/starter-code/basic-algorithms.js
@@ -0,0 +1,66 @@
+// Names and Input
+
+//DRIVER NAME
+var hacker1 = prompt ('Driver name?')
+    alert ('The driver\'s name is ' + hacker1)
+
+//NAVIGATOR NAME
+var hacker2 = prompt ('Navigator\'s name?')
+    alert ('The navigator\'s name is ' + hacker2)
+
+
+//Conditionals
+if (hacker1.length > hacker2.length) {
+alert ('The Driver has the longest name, it has ' + hacker1.length + ' characters')
+}
+else if (hacker1.length < hacker2.length) {
+alert ('Yo, navigator got the longest name, it has ' + hacker2.length + ' characters')
+} 
+else {
+alert ('wow, you both got equally long names, ' + hacker1.length + ' characters')
+}
+
+//LOOPS
+var upperCase = '';
+for ( var i = 0; i < hacker1.length; i++ ) {
+    upperCase += hacker1[i].toLocaleUpperCase() + ' ';
+}
+alert (upperCase);
+
+var inverseCase = '';
+for (var i = hacker2.length-1; i >=0; i--) {
+    inverseCase += hacker2[i];
+}
+alert (inverseCase);
+
+for ( var i = 0; i < hacker1.length; i++ ) {
+    if(hacker1[i].localeCompare(hacker2[i]) === 0){
+        i++
+    }
+    else if(hacker1[i].localeCompare(hacker2[i]) < 0){
+        alert('The driver\'s name goes first')
+        
+    } 
+    else if (hacker1[i].localeCompare(hacker2[i]) > 0) {
+        alert('Yo,the navigator goes first definitely')
+    } 
+    else {
+        alert('What?! You both got the same name?')
+    }
+}
+
+var hacker1Length = 0;
+for (var i = 0; i < hacker1.length; i++) {
+  if ( hacker1[i] === hacker2[i] ) {
+    hacker1Length++ ;
+    if (hacker1Length == hacker1.length){
+      console.log ("what? you both got the same name")
+    }
+  } else if (hacker1[i] < hacker2[i]){
+    console.log("The driver's name goes first");
+    break;
+  } else if (hacker1[i] > hacker2[i]){
+    console.log("No, The navigator's goes first, definitely");
+    break;
+  }
+}
